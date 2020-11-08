b40152da455809388873cb19d0af697c887b5d16
bakuryukhanh <tvknbk2015@gmail.com>
2020-11-08 19:30:48 +0700

    moddified to coffee web

--- public/js/contact-form-script.js
+++ public/js/contact-form-script.js
@@ -20,19 +20,27 @@ function submitForm() {
     var message = $("#message").val();
 
     //Submit form
-    // $.ajax({
-    //     type: "POST",
-    //     url: "php/form-process.php",
-    //     data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
-    //     success : function(text){
-    //         if (text == "success"){
-    //             formSuccess();
-    //         } else {
-    //             formError();
-    //             submitMSG(false,text);
-    //         }
-    //     }
-    // });
+    $.ajax({
+        type: "POST",
+        url: "php/form-process.php",
+        data:
+            "name=" +
+            name +
+            "&email=" +
+            email +
+            "&msg_subject=" +
+            msg_subject +
+            "&message=" +
+            message,
+        success: function (text) {
+            if (text == "success") {
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false, text);
+            }
+        },
+    });
 }
 
 function formSuccess() {
